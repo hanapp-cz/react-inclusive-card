74de502148e8e7d9f90fb74474318c8b9d84ef6a
Hana Pospíšilová <h.hedbavna@gmail.com>
2025-06-09 11:15:25 +0200

    docs: :memo: add comments & props

--- src/Card.tsx
+++ src/Card.tsx
@@ -4,61 +4,66 @@ import * as React from "react";
 
 type TRequiredChildren = Required<React.PropsWithChildren<object>>;
 
-type TCardProps<T extends React.ElementType = "article"> = {
-  as?: T;
-  className?: string;
-  contentClassName?: string;
-  imageClassName?: string;
-  imageProps: TImageProps;
-} & TRequiredChildren &
-  Omit<React.ComponentPropsWithoutRef<T>, "as" | "children" | "className">;
-
-type TLinkProps =
-  | {
-      customLinkComponent: React.ElementType;
-      href?: never;
-    }
-  | {
-      customLinkComponent?: never;
-      href: string;
-    };
-
-type TCardTitleProps = TRequiredChildren &
-  TLinkProps & {
-    as?: React.ElementType;
-    className?: string;
-  };
-
 type TImageProps =
   | {
+      /**
+       * The source URL of the image
+       */
       src: string;
       /**
        * If your image is decorative, pass and empty string
        */
       alt: string;
+
       customImageComponent?: never;
     }
   | {
       src?: never;
       alt?: never;
+      /**
+       * A custom image component to render instead of a regular image
+       */
       customImageComponent: React.ReactElement;
     };
 
-type TCardImageProps = TImageProps & {
-  className?: string;
-};
+type TOtherProps<TElement extends React.ElementType> = Omit<
+  React.ComponentPropsWithoutRef<TElement>,
+  "as" | "children" | "className"
+>;
 
-type TCardControlsProps = {
-  children: React.ReactNode & {
-    props?: {
+type TCardProps<TElement extends React.ElementType = "article"> =
+  TRequiredChildren &
+    TOtherProps<TElement> & {
+      /**
+       * The element type of the card,
+       * @default "article"
+       */
+      as?: TElement;
       className?: string;
+      /**
+       * extend the `className` of the card content area
+       * @default "inclusive-card__content"
+       */
+      contentClassName?: string;
+      /**
+       * extend the `className` of the card image area
+       * @default "inclusive-card__image"
+       */
+      imageClassName?: string;
+      /**
+       * Pass either `src` and `alt` or a custom image component.
+       * - If you want to use a custom image component, pass it as `customImageComponent` prop.
+       * - If you want to use a regular image, pass `src` and `alt` props.
+       */
+      imageProps: TImageProps;
     };
-  };
-  as?: React.ElementType;
-  className?: string;
-  asChild?: boolean;
-};
 
+/**
+ * The Card component is a flexible and accessible card layout component
+ *
+ * It consists of an image and content area.
+ * you can specify the element type for the card using the `as` prop.
+ */
 const Card = <TElement extends React.ElementType>({
   as,
   className = "",
@@ -76,27 +81,70 @@ const Card = <TElement extends React.ElementType>({
         {children}
       </div>
       <div className={`inclusive-card__image ${imageClassName}`}>
-        <CardImage {...imageProps} />
+        {imageProps.customImageComponent}
+        {!imageProps.customImageComponent && (
+          <img src={imageProps.src} alt={imageProps.alt} />
+        )}
       </div>
     </Component>
   );
 };
 
-const CardTitle: React.FC<TCardTitleProps> = ({
-  as = "h2",
+// * CARD TITLE ----------------------------------------------
+type TLinkProps =
+  | {
+      customLinkComponent: React.ElementType;
+      href?: never;
+      linkProps?: never;
+    }
+  | {
+      customLinkComponent?: never;
+      href: string;
+      linkProps?: Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "href">;
+    };
+
+type TCardTitleProps<
+  TElement extends Extract<
+    React.ElementType,
+    "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+  >
+> = TRequiredChildren &
+  TOtherProps<TElement> &
+  TLinkProps & {
+    as?: TElement;
+    className?: string;
+  };
+
+/**
+ * @see {@link Card.Title} for more details
+ */
+const CardTitle = <
+  TElement extends Extract<
+    React.ElementType,
+    "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+  >
+>({
+  as,
+  href,
   children,
   className = "",
   customLinkComponent,
-  href,
-}) => {
-  const Component = as;
+  "aria-describedby": ariaDescribedBy,
+  linkProps = {},
+  ...rest
+}: TCardTitleProps<TElement>) => {
+  const Component = as ?? "h2";
 
   const LinkComponent = customLinkComponent ? customLinkComponent : "a";
   const hrefProp = href ? { href } : {};
 
   return (
-    <Component className={`inclusive-card__title ${className}`}>
-      <LinkComponent {...hrefProp}>
+    <Component className={`inclusive-card__title ${className}`} {...rest}>
+      <LinkComponent
+        aria-describedby={ariaDescribedBy}
+        {...hrefProp}
+        {...linkProps}
+      >
         {children}
         <span className="inclusive-card__link-area" />
       </LinkComponent>
@@ -104,27 +152,29 @@ const CardTitle: React.FC<TCardTitleProps> = ({
   );
 };
 
-const CardImage: React.FC<TCardImageProps> = ({
-  customImageComponent,
-  src,
-  alt,
-}) => {
-  return (
-    <>
-      {customImageComponent}
-
-      {!customImageComponent && (
-        <img src={src} alt={alt} className="inclusive-card__image" />
-      )}
-    </>
-  );
+// * CARD CONTROLS -----------------------------------------
+type TChildrenWithProps = React.ReactNode & {
+  props?: {
+    className?: string;
+  };
 };
 
+type TCardControlsProps = TOtherProps<"div"> & {
+  children: TChildrenWithProps;
+  as?: React.ElementType;
+  className?: string;
+  asChild?: boolean;
+};
+
+/**
+ * @see {@link Card.Controls} for more details
+ */
 const CardControls: React.FC<TCardControlsProps> = ({
   as = "div",
   children,
   className = "",
   asChild = false,
+  ...rest
 }) => {
   if (asChild) {
     try {
@@ -168,30 +218,103 @@ const CardControls: React.FC<TCardControlsProps> = ({
   const Component = as;
 
   return (
-    <Component className={`inclusive-card__controls ${className}`}>
+    <Component className={`inclusive-card__controls ${className}`} {...rest}>
       {children}
     </Component>
   );
 };
 
-type TCallToActionProps = {
+// * CALL TO ACTION -----------------------------------------
+
+type TSpanProps = React.HTMLAttributes<HTMLSpanElement>;
+type TCallToActionProps = TSpanProps & {
   children: React.ReactNode;
   className?: string;
 };
 
+/**
+ * @see {@link Card.CallToAction} for more details
+ */
 const CardCallToAction: React.FC<TCallToActionProps> = ({
   children,
   className = "",
+  ...rest
 }) => {
   return (
-    <span className={`inclusive-card__cta ${className}`} aria-hidden="true">
+    <span
+      className={`inclusive-card__cta ${className}`}
+      aria-hidden="true"
+      {...rest}
+    >
       {children}
     </span>
   );
 };
 
+// * EXPORTS -------------------------------------------------
+
 export { Card };
 
+/**
+ * The `<Card.Title>` component is used to render the title of the card,
+ * it contains a link and it makes the whole card clickable.
+ *
+ * You can pass a custom link component using the `customLinkComponent` prop,
+ * or just pass a `href` prop to use the default `<a>` element.
+ */
 Card.Title = CardTitle;
+
+/**
+ * Wrap your interactive elements (like buttons and links) with `<Card.Controls>`
+ * to raise them above the card clickable area.
+ *
+ * It renders a `<div>` by default,
+ * - you can either change it to any other element using the `as` prop
+ * - or you can use the `asChild` prop to render the child element instead of the `<div>`
+ *
+ * ! Only one child element is allowed for the `<Card.Controls>` component when `asChild` is set to `true`
+ */
 Card.Controls = CardControls;
+
+/**
+ * Wrap your "call to action" element with `<Card.CallToAction>` to hide it from screen readers.
+ * With clickable card, the call-to-action element should be rather decorative, providing additional context to the card link.
+ *
+ * The Card.CallToAction component gets focus styles when the `<Card.Title>` link is focused.
+ * You can customize a `box-shadow` & `text-decoration` using CSS custom properties:
+ * - `--inclusive-card-cta-focus-decoration`
+ * - `--inclusive-card-cta-focus-shadow`
+ *
+ * Or you can override default styles in your CSS file using selector:
+ * ```css
+ * .inclusive-card__title:focus-within ~ .inclusive-card__cta {
+ *  Your custom styles here
+ * }
+ * ```
+ *
+ * You can add an `aria-describedby` attribute to the Card.Title to attach a call-to-action text to the card link.
+ *
+ * @example with default `<Card.Title>` link:
+ * ```jsx
+ * <Card as="article">
+ *    <Card.Title href="/some-link" aria-describedby="desc-example-title">Example Title</Card.Title>
+ *    <Card.CallToAction id="desc-example-title">Click here to learn more</Card.CallToAction>
+ * </Card>
+ * ```
+ *
+ * @example with custom `<Card.Title>` link:
+ * ```jsx
+ * const MyCustomLink = ({ children, ...props }) => (
+ *   <Link aria-describedby="desc-example-title" {...props}>
+ *     {children}
+ *   </Link>
+ * );
+ *
+ * <Card as="article">
+ *    <Card.Title customLinkComponent={MyCustomLink}>
+ *      Example Title
+ *    </Card.Title>
+ *    <Card.CallToAction id="desc-example-title">Click here to learn more</Card.CallToAction>
+ * </Card>
+ */
 Card.CallToAction = CardCallToAction;
